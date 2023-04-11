1d4b1b076241060ee082a7f9eda0e735f5343a8c
Nastja0 <anastasizta_mgn3003@inbox.ru>
2023-04-11 20:54:00 +0500

    update

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import ttest_1samp
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+chat_id = 522929689 # Ваш chat ID, не меняйте название переменной
+
+def solution(x) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p = 0.04
+    return ttest_1samp(x, 300).pvalue <= p # Ваш ответ, True или False
